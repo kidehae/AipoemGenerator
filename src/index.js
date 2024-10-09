0fbff4c0ef0567adb44f53ed48b6601b381020e8
kidehae <149243635+kidehae@users.noreply.github.com>
2024-10-09 07:38:52 +0300

    doing-the-final-touch

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ function displayPoem(response) {
 function generatingPoem(events) {
   events.preventDefault();
   let theTopic = document.querySelector("#topic");
-  let prompt = ` Write a poem about ${theTopic} `;
+  theTopic = theTopic.value;
+  let prompt = ` Write an poem about ${theTopic} `;
   //let context = `you are an AI professinal poem writer who writes a poem based on ${prompt} in HTMLS format separating each line with break sstatment <br> and signing the poem at the end of the poem as <strong> SheCodes AI  </strong>`;
-  let context = `you are a romantic poem expert who love to write short poems. your mission is to genertes a four line poem about ${theTopic} in basic HTML and separate each line with </br>.you must not write outside the ${theTopic}. make sure to follow ${prompt}. Do not include a title to the poem.The poem must not be greater the 4 lines. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem`;
+  let context = `you are a romantic poem expert who love to write short poems. your mission is to genertes a four line poem about ${theTopic} in basic HTML and separate each line with </br>.you must not write outside the ${theTopic}. make sure to follow ${prompt}.Don not use the word html and do not enclose it with back tics. Do not include a title to the poem.The poem must not be greater the 4 lines. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem`;
   let apiKey = "4f4b7dfae223376b869d41110t4o0071";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#thePoem");
+  poemElement.classList.remove("noPoem");
+  poemElement.innerHTML = `<div class = "blink_me">Generating a poem for you on the topic ${theTopic}</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 33;
